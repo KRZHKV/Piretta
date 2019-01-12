4c348b49f18bf0eb527fddb9be18b74bc186b051
Kir <Kruzhkovll@gmail.com>
2019-01-12 15:22:39 +0300

    header

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ $(document).ready(function(){
         
     });
 });
+
+$(document).ready(function(){
+    var link = $('.menu-link');
+    var link_active = $('.menu-link-active');
+    var menu = $('.nav_menu_adaptive');
+
+
+    link.click(function(){
+        link.toggleClass('menu-link-active');
+        menu.toggleClass('nav_menu_adaptive-active');
+    });
+    link_active.click(function(){
+        link.removeClass('menu-link-active');
+        menu.removeClass('nav_menu_adaptive-active');
+    });
+
+});

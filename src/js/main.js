120fa1f67fae595be74391d750b05c56c666d934
Sitvok <Kruzhkovll@gmail.com>
2019-02-25 14:00:01 +0300

    govno

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,26 @@
 $(document).ready(function(){
-    $('.news-slider').slick({
-        infinite: true,
-        variableWidth: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<button class="slider_button_prew"></button>',
-        nextArrow: '<button class="slider_button_next"></button>',
-        responsive: [
-            {
-              breakpoint: 1100,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-              }
-            },
-            {
-              breakpoint: 870,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
-            },
-            {
-              breakpoint: 500,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-   
-    });
+  $('.news-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slider_button_prew"></button>',
+    nextArrow: '<button class="slider_button_next></button>',
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
 
 $(document).ready(function(){
